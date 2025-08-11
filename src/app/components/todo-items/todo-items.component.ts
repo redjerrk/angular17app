97d737b8a78cef525a04b739d38d247ca4f6e4df
Rakibul Islam <rakib.islam.cse@gmail.com>
2025-08-11 17:41:09 +0600

    nav bar updated

---
+++ src/app/components/todo-items/todo-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-items.component.html',
+  styleUrl: './todo-items.component.css'
+})
+export class TodoItemsComponent {
+
+}
